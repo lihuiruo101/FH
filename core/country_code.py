314835e0bc9f79d7e8aa7d63785ffa5cce7ad463
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 16:05:17 +0900

    Create country_code.py

---
+++ core/country_code.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+import enum
+from bcps.cli import dialog_creator
+from bcps import core
+
+
+class CountryCodeType(enum.Enum):
+    EN = "en"
+    JP = "jp"
+    KR = "kr"
+    TW = "tw"
+
+
+class CountryCode:
+    def __init__(self, cc: str | CountryCodeType):
+        self.value = cc.value if isinstance(cc, CountryCodeType) else cc
+        self.value = self.value.lower()
+
+    def get_code(self) -> str:
+        return self.value
+
+    def get_client_info_code(self) -> str:
+        code = self.get_code()
+        if code == "jp":
+            return "ja"
+        return code
+
+    def get_patching_code(self) -> str:
+        code = self.get_code()
+        if code == "jp":
+            return ""
+        return code
+
+    @staticmethod
+    def from_patching_code(code: str) -> CountryCode:
+        if code == "":
+            return CountryCode(CountryCodeType.JP)
+        return CountryCode(code)
+
+    @staticmethod
+    def from_code(code: str) -> CountryCode:
+        return CountryCode(code)
+
+    @staticmethod
+    def get_all() -> list["CountryCode"]:
+        return [CountryCode(cc) for cc in CountryCodeType]
+
+    @staticmethod
+    def get_all_str() -> list[str]:
+        ccts = CountryCode.get_all()
+        return [cc.get_code() for cc in ccts]
+
+    def __str__(self) -> str:
+        return self.get_code()
+
+    def __repr__(self) -> str:
+        return self.get_code()
+
+    def copy(self) -> CountryCode:
+        return self
+
+    @staticmethod
+    def select() -> CountryCode | None:
+        index = dialog_creator.ChoiceInput.from_reduced(
